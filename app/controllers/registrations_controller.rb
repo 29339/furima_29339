a329a6b149631dce3cfd3035f76f11b5df23a921
29339 <katomanzu86@gmail.com>
2020-08-26 09:24:42 +0900

    ユーザー管理機能実装

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,22 @@
+class RegistrationsController < ApplicationController
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.valid? 
+			@user.save 
+			redirect_to index
+		else
+			render :new
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_day)
+	end
+
+end
+
