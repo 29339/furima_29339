861d5ec1850bd1bd6fe38a10a4f1e95e3db6c625
29339 <katomanzu86@gmail.com>
2020-08-27 13:41:12 +0900

    ユーザー管理機能　修正(2)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :birth_day, presence: true
-
-  PASSWORD_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
-  validates_format_of :first_name, :last_name, :first_name_kana, :last_name_kana, with: PASSWORD_REGEX, message: 'Full-width katakana characters'
-
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+  with_options presence: true do
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "Include both letters and numbers"}
+    validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
+    validates :first_name_kana, :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+  end
 end
