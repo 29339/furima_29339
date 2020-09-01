a03472fe337a8e02df01ec1467b3b0069bf93ff0
29339 <katomanzu86@gmail.com>
2020-09-01 20:54:04 +0900

    商品商品情報編集機能実装修正(2)

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'items#index'
-<<<<<<< Updated upstream
-  get '/sing_in', to: 'sessions#new'
-  get '/sign_up', to: 'registrations#new'
   devise_for :users
-  resources :items, only: [:new, :create, :index]
-=======
+  root to: 'items#index'
   resources :items, only: [:new, :create, :index, :show, :edit, :update]
->>>>>>> Stashed changes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
